e7f08651f1f9cb05a4a542e843ac9298050d2f75
junkai <576327324@qq.com>
2020-11-23 13:08:01 +0800

    修改生成结构，更改为json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jsesc = require("jsesc");
 
 const toJs = (curlCommand) => {
   const request = util.parseCurlCommand(curlCommand);
-  let jsCode = "";
+  let jsCode = {};
 
   if (request.data === true) {
     request.data = "";
@@ -16,7 +16,6 @@ const toJs = (curlCommand) => {
     if (request.data.indexOf("'") > -1) {
       request.data = jsesc(request.data);
     }
-
     try {
       JSON.parse(request.data);
 
@@ -30,40 +29,59 @@ const toJs = (curlCommand) => {
 
       request.data = "JSON.stringify(" + request.data + ")";
     } catch {
-      request.data = '"' + request.data + '"';
+      request.data =  request.data;
     }
   }
-
-  jsCode += '{\n    "url": "' + request.url + '"';
+  jsCode.url = request.url;
+  // jsCode += '{\n    "url": "' + request.url + '"';
   if (request.static) {
-    jsCode += ',\n    "static": "' + request.static + '"';
+    jsCode.static = request.static;
+
+    // jsCode += ',\n    "static": "' + request.static + '"';
   }
   if (request.dynamic) {
-    jsCode += ',\n    "dynamic": "' + request.dynamic + '"';
+    jsCode.dynamic = request.dynamic;
+
+    // jsCode += ',\n    "dynamic": "' + request.dynamic + '"';
   }
   if (request.title) {
-    jsCode += ',\n    "title": "' + request.title + '"';
+    jsCode.title = request.title;
+
+    // jsCode += ',\n    "title": "' + request.title + '"';
   }
   if (request.dialog) {
-    jsCode += ',\n    "dialog": "' + request.dialog + '"';
+    jsCode.dialog = request.dialog;
+
+    // jsCode += ',\n    "dialog": "' + request.dialog + '"';
   }
   if (request.close) {
-    jsCode += ',\n    "close": "' + request.close + '"';
+    jsCode.close = request.close;
+
+    // jsCode += ',\n    "close": "' + request.close + '"';
   }
   if (request.back) {
-    jsCode += ',\n    "back": "' + request.back + '"';
+    jsCode.back = request.back;
+
+    // jsCode += ',\n    "back": "' + request.back + '"';
   }
   if (request.refresh) {
-    jsCode += ',\n    "refresh": "' + request.refresh + '"';
+    jsCode.refresh = request.refresh;
+
+    // jsCode += ',\n    "refresh": "' + request.refresh + '"';
   }
   if (request.success) {
-    jsCode += ',\n    "success": "' + request.success + '"';
+    jsCode.success = request.success;
+
+    // jsCode += ',\n    "success": "' + request.success + '"';
   }
   if (request.fail) {
-    jsCode += ',\n    "fail": "' + request.fail + '"';
+    jsCode.fail = request.fail;
+
+    // jsCode += ',\n    "fail": "' + request.fail + '"';
   }
-  jsCode += ",\n";
-  jsCode += '    "method": "' + request.method.toUpperCase() + '"';
+  // jsCode += ",\n";
+  // jsCode += '    "method": "' + request.method.toUpperCase() + '"';
+  jsCode.method = request.method.toUpperCase();
 
   if (request.headers || request.cookies || request.auth || request.body) {
     // jsCode += ',\n'
@@ -107,26 +125,35 @@ const toJs = (curlCommand) => {
     }
   }
   if (request.data) {
-    jsCode += ',\n    "body": ' + request.data;
+    jsCode.body = request.data
+    // jsCode += ',\n    "body": ' + request.data;
   }
   if (request.context) {
-    jsCode += ',\n    "context": "' + request.context + '"';
+    jsCode.context = request.context
+
+    // jsCode += ',\n    "context": "' + request.context + '"';
   }
   if (request.alert) {
-    jsCode += ',\n    "alert": "' + request.alert + '"';
+    jsCode.alert = request.alert
+    
+    // jsCode += ',\n    "alert": "' + request.alert + '"';
   }
   if (request.export) {
-    jsCode += ',\n    "export": "' + request.export + '"';
+    jsCode.export = request.export
+
+    // jsCode += ',\n    "export": "' + request.export + '"';
   }
   if (request["export-name"]) {
-    jsCode += ',\n    "export-name": "' + request["export-name"] + '"';
+    jsCode["export-name"] = request["export-name"]
+
+    // jsCode += ',\n    "export-name": "' + request["export-name"] + '"';
   }
 
-  jsCode += "\n";
+  // jsCode += "\n";
 
-  jsCode += "}";
+  // jsCode += "}";
 
-  return jsCode + "\n";
+  return jsCode;
 };
 
 module.exports = toJs;
--- util.js
+++ util.js
@@ -1,280 +1,305 @@
-const cookie = require('cookie')
-const yargs = require('yargs')
-const URL = require('url')
-const querystring = require('query-string')
+const cookie = require("cookie");
+const yargs = require("yargs");
+const URL = require("url");
+const querystring = require("query-string");
 
-const parseCurlCommand = curlCommand => {
+const parseCurlCommand = (curlCommand) => {
   // Remove newlines (and from continuations)
-  curlCommand = curlCommand.replace(/\\\r|\\\n/g, '')
+  curlCommand = curlCommand.replace(/\\\r|\\\n/g, "");
 
   // Remove extra whitespace
-  curlCommand = curlCommand.replace(/\s+/g, ' ')
+  curlCommand = curlCommand.replace(/\s+/g, " ");
 
   // yargs parses -XPOST as separate arguments. just prescreen for it.
-  curlCommand = curlCommand.replace(/ -XPOST/, ' -X POST')
-  curlCommand = curlCommand.replace(/ -XGET/, ' -X GET')
-  curlCommand = curlCommand.replace(/ -XPUT/, ' -X PUT')
-  curlCommand = curlCommand.replace(/ -XPATCH/, ' -X PATCH')
-  curlCommand = curlCommand.replace(/ -XDELETE/, ' -X DELETE')
+  curlCommand = curlCommand.replace(/ -XPOST/, " -X POST");
+  curlCommand = curlCommand.replace(/ -XGET/, " -X GET");
+  curlCommand = curlCommand.replace(/ -XPUT/, " -X PUT");
+  curlCommand = curlCommand.replace(/ -XPATCH/, " -X PATCH");
+  curlCommand = curlCommand.replace(/ -XDELETE/, " -X DELETE");
   // Safari adds `-Xnull` if is unable to determine the request type, it can be ignored
-  curlCommand = curlCommand.replace(/ -Xnull/, ' ')
-  curlCommand = curlCommand.trim()
-
+  curlCommand = curlCommand.replace(/ -Xnull/, " ");
+  curlCommand = curlCommand.trim();
+  console.log(curlCommand);
   // Parse with some understanding of the meanings of flags.  In particular,
   // boolean flags can be trouble if the URL to fetch follows immediately
   // after, since it will be taken as an argument to the flag rather than
   // interpreted as a positional argument.  Someone should add all the flags
   // likely to cause trouble here.
+
   const parsedArguments = yargs
-    .boolean(['I', 'head', 'compressed', 'L', 'k', 'silent', 's','static','dynamic','dialog','close','back','refresh'])
-    .alias('H', 'header')
-    .alias('A', 'user-agent')
-    .parse(curlCommand)
-  let cookieString
-  let cookies
-  let url = parsedArguments._[1]
+    .boolean([
+      "I",
+      "head",
+      "compressed",
+      "L",
+      "k",
+      "silent",
+      "s",
+      "static",
+      "dynamic",
+      "dialog",
+      "close",
+      "back",
+      "refresh",
+    ])
+    .alias("H", "header")
+    .alias("A", "user-agent")
+    .parse(curlCommand);
+  let cookieString;
+  let cookies;
+  let url = parsedArguments._[1];
 
   // if url argument wasn't where we expected it, try to find it in the other arguments
   if (!url) {
     for (const argName in parsedArguments) {
-      if (typeof parsedArguments[argName] === 'string') {
-        if (parsedArguments[argName].indexOf('http') === 0 || parsedArguments[argName].indexOf('www.') === 0) {
-          url = parsedArguments[argName]
+      if (typeof parsedArguments[argName] === "string") {
+        if (
+          parsedArguments[argName].indexOf("http") === 0 ||
+          parsedArguments[argName].indexOf("www.") === 0
+        ) {
+          url = parsedArguments[argName];
         }
       }
     }
   }
-  let headers
-
+  let headers;
   if (parsedArguments.header) {
     if (!headers) {
-      headers = {}
+      headers = {};
     }
     if (!Array.isArray(parsedArguments.header)) {
-      parsedArguments.header = [parsedArguments.header]
+      parsedArguments.header = [parsedArguments.header];
     }
-    parsedArguments.header.forEach(header => {
-      if (header.indexOf('Cookie') !== -1) {
-        cookieString = header
+    parsedArguments.header.forEach((header) => {
+      if (header.indexOf("Cookie") !== -1) {
+        cookieString = header;
       } else {
-        const components = header.split(/:(.*)/)
+        const components = header.split(/:(.*)/);
         if (components[1]) {
-          headers[components[0]] = components[1].trim()
+          headers[components[0]] = components[1].trim();
         }
       }
-    })
+    });
   }
 
-  if (parsedArguments['user-agent']) {
+  if (parsedArguments["user-agent"]) {
     if (!headers) {
-      headers = {}
+      headers = {};
     }
-    headers['User-Agent'] = parsedArguments['user-agent']
+    headers["User-Agent"] = parsedArguments["user-agent"];
   }
 
   if (parsedArguments.b) {
-    cookieString = parsedArguments.b
+    cookieString = parsedArguments.b;
   }
   if (parsedArguments.cookie) {
-    cookieString = parsedArguments.cookie
+    cookieString = parsedArguments.cookie;
   }
-  let multipartUploads
+  let multipartUploads;
   if (parsedArguments.F) {
-    multipartUploads = {}
+    multipartUploads = {};
     if (!Array.isArray(parsedArguments.F)) {
-      parsedArguments.F = [parsedArguments.F]
+      parsedArguments.F = [parsedArguments.F];
     }
-    parsedArguments.F.forEach(multipartArgument => {
+    parsedArguments.F.forEach((multipartArgument) => {
       // input looks like key=value. value could be json or a file path prepended with an @
-      const splitArguments = multipartArgument.split('=', 2)
-      const key = splitArguments[0]
-      const value = splitArguments[1]
-      multipartUploads[key] = value
-    })
+      const splitArguments = multipartArgument.split("=", 2);
+      const key = splitArguments[0];
+      const value = splitArguments[1];
+      multipartUploads[key] = value;
+    });
   }
   if (cookieString) {
     const cookieParseOptions = {
-      decode: function (s) { return s }
-    }
+      decode: function (s) {
+        return s;
+      },
+    };
     // separate out cookie headers into separate data structure
     // note: cookie is case insensitive
-    cookies = cookie.parse(cookieString.replace(/^Cookie: /gi, ''), cookieParseOptions)
+    cookies = cookie.parse(
+      cookieString.replace(/^Cookie: /gi, ""),
+      cookieParseOptions
+    );
   }
-  let method
-  if (parsedArguments.X === 'GET') {
-    method = 'get'
-  } else if (parsedArguments.X === 'POST') {
-    method = 'post'
-  } else if (parsedArguments.X === 'PUT' ||
-    parsedArguments.T) {
-    method = 'put'
-  } else if (parsedArguments.X === 'PATCH') {
-    method = 'patch'
-  } else if (parsedArguments.X === 'DELETE') {
-    method = 'delete'
-  } else if (parsedArguments.X === 'OPTIONS') {
-    method = 'options'
-  } else if ((parsedArguments.d ||
-    parsedArguments.data ||
-    parsedArguments['data-ascii'] ||
-    parsedArguments['data-binary'] ||
-    parsedArguments['data-raw'] ||
-    parsedArguments.F ||
-    parsedArguments.form) && !((parsedArguments.G || parsedArguments.get))) {
-    method = 'post'
-  } else if (parsedArguments.I ||
-    parsedArguments.head) {
-    method = 'head'
+  let method;
+  if (parsedArguments.X === "GET") {
+    method = "get";
+  } else if (parsedArguments.X === "POST") {
+    method = "post";
+  } else if (parsedArguments.X === "PUT" || parsedArguments.T) {
+    method = "put";
+  } else if (parsedArguments.X === "PATCH") {
+    method = "patch";
+  } else if (parsedArguments.X === "DELETE") {
+    method = "delete";
+  } else if (parsedArguments.X === "OPTIONS") {
+    method = "options";
+  } else if (
+    (parsedArguments.d ||
+      parsedArguments.data ||
+      parsedArguments["data-ascii"] ||
+      parsedArguments["data-binary"] ||
+      parsedArguments["data-raw"] ||
+      parsedArguments.F ||
+      parsedArguments.form) &&
+    !(parsedArguments.G || parsedArguments.get)
+  ) {
+    method = "post";
+  } else if (parsedArguments.I || parsedArguments.head) {
+    method = "head";
   } else {
-    method = 'get'
+    method = "get";
   }
 
-  const compressed = !!parsedArguments.compressed
-  const urlObject = URL.parse(url) // eslint-disable-line
+  const compressed = !!parsedArguments.compressed;
+  const urlObject = URL.parse(url); // eslint-disable-line
   // if GET request with data, convert data to query string
   // NB: the -G flag does not change the http verb. It just moves the data into the url.
+
   if (parsedArguments.G || parsedArguments.get) {
-    urlObject.query = urlObject.query ? urlObject.query : ''
-    const option = 'd' in parsedArguments ? 'd' : 'data' in parsedArguments ? 'data' : null
+    urlObject.query = urlObject.query ? urlObject.query : "";
+    const option =
+      "d" in parsedArguments ? "d" : "data" in parsedArguments ? "data" : null;
     if (option) {
-      let urlQueryString = ''
+      let urlQueryString = "";
 
-      if (url.indexOf('?') < 0) {
-        url += '?'
+      if (url.indexOf("?") < 0) {
+        url += "?";
       } else {
-        urlQueryString += '&'
+        urlQueryString += "&";
       }
 
-      if (typeof (parsedArguments[option]) === 'object') {
-        urlQueryString += parsedArguments[option].join('&')
+      if (typeof parsedArguments[option] === "object") {
+        urlQueryString += parsedArguments[option].join("&");
       } else {
-        urlQueryString += parsedArguments[option]
+        urlQueryString += parsedArguments[option];
       }
-      urlObject.query += urlQueryString
-      url += urlQueryString
-      delete parsedArguments[option]
+      urlObject.query += urlQueryString;
+      url += urlQueryString;
+      delete parsedArguments[option];
     }
   }
-  if (urlObject.query && urlObject.query.endsWith('&')) {
-    urlObject.query = urlObject.query.slice(0, -1)
+  if (urlObject.query && urlObject.query.endsWith("&")) {
+    urlObject.query = urlObject.query.slice(0, -1);
   }
-  const query = querystring.parse(urlObject.query, { sort: false })
+  const query = querystring.parse(urlObject.query, { sort: false });
   for (const param in query) {
     if (query[param] === null) {
-      query[param] = ''
+      query[param] = "";
     }
   }
 
-  urlObject.search = null // Clean out the search/query portion.
+  urlObject.search = null; // Clean out the search/query portion.
   const request = {
     url: url,
-    urlWithoutQuery: URL.format(urlObject)
-  }
+    urlWithoutQuery: URL.format(urlObject),
+  };
   if (compressed) {
-    request.compressed = true
+    request.compressed = true;
   }
 
   if (Object.keys(query).length > 0) {
-    request.query = query
+    request.query = query;
   }
   if (headers) {
-    request.headers = headers
+    request.headers = headers;
   }
-  request.method = method
+  request.method = method;
 
   if (cookies) {
-    request.cookies = cookies
-    request.cookieString = cookieString.replace('Cookie: ', '')
+    request.cookies = cookies;
+    request.cookieString = cookieString.replace("Cookie: ", "");
   }
   if (multipartUploads) {
-    request.multipartUploads = multipartUploads
+    request.multipartUploads = multipartUploads;
   }
   if (parsedArguments.data) {
-    request.data = parsedArguments.data
-  } else if (parsedArguments['data-binary']) {
-    request.data = parsedArguments['data-binary']
-    request.isDataBinary = true
+    request.data = parsedArguments.data;
+  } else if (parsedArguments["data-binary"]) {
+    request.data = parsedArguments["data-binary"];
+    request.isDataBinary = true;
   } else if (parsedArguments.d) {
-    request.data = parsedArguments.d
-  } else if (parsedArguments['data-ascii']) {
-    request.data = parsedArguments['data-ascii']
-  } else if (parsedArguments['data-raw']) {
-    request.data = parsedArguments['data-raw']
-    request.isDataRaw = true
+    request.data = parsedArguments.d;
+  } else if (parsedArguments["data-ascii"]) {
+    request.data = parsedArguments["data-ascii"];
+  } else if (parsedArguments["data-raw"]) {
+    request.data = parsedArguments["data-raw"];
+    request.isDataRaw = true;
   }
 
   if (parsedArguments.u) {
-    request.auth = parsedArguments.u
+    request.auth = parsedArguments.u;
   }
   if (parsedArguments.user) {
-    request.auth = parsedArguments.user
+    request.auth = parsedArguments.user;
   }
-  if(parsedArguments.context) {
-    request.context = parsedArguments.context
+  if (parsedArguments.context) {
+    request.context = parsedArguments.context;
   }
-  if(parsedArguments.alert) {
-    request.alert = parsedArguments.alert
+  if (parsedArguments.alert) {
+    request.alert = parsedArguments.alert;
   }
-  if(parsedArguments.title) {
-    request.title = parsedArguments.title
+  if (parsedArguments.title) {
+    request.title = parsedArguments.title;
   }
-  if(parsedArguments.dialog) {
-    request.dialog = parsedArguments.dialog
+  if (parsedArguments.dialog) {
+    request.dialog = parsedArguments.dialog;
   }
-  if(parsedArguments.export) {
-    request.export = parsedArguments.export
+  if (parsedArguments.export) {
+    request.export = parsedArguments.export;
   }
-  if(parsedArguments['export-name']) {
-    request['export-name'] = parsedArguments['export-name']
+  if (parsedArguments["export-name"]) {
+    request["export-name"] = parsedArguments["export-name"];
   }
-  if(parsedArguments.static){
+  if (parsedArguments.static) {
     request.static = parsedArguments.static;
   }
-  if(parsedArguments.dynamic){
-    request.dynamic = parsedArguments.dynamic
+  if (parsedArguments.dynamic) {
+    request.dynamic = parsedArguments.dynamic;
   }
-  if(parsedArguments.close){
-    request.close = parsedArguments.close
+  if (parsedArguments.close) {
+    request.close = parsedArguments.close;
   }
-  if(parsedArguments.back){
-    request.back = parsedArguments.back
+  if (parsedArguments.back) {
+    request.back = parsedArguments.back;
   }
-  if(parsedArguments.refresh){
-    request.refresh = parsedArguments.refresh
+  if (parsedArguments.refresh) {
+    request.refresh = parsedArguments.refresh;
   }
-  if(parsedArguments.success){
-    request.success = parsedArguments.success
+  if (parsedArguments.success) {
+    request.success = parsedArguments.success;
   }
-  if(parsedArguments.fail){
-    request.fail = parsedArguments.fail
+  if (parsedArguments.fail) {
+    request.fail = parsedArguments.fail;
   }
   if (Array.isArray(request.data)) {
-    request.dataArray = request.data
-    request.data = request.data.join('&')
+    request.dataArray = request.data;
+    request.data = request.data.join("&");
   }
 
   if (parsedArguments.k || parsedArguments.insecure) {
-    request.insecure = true
+    request.insecure = true;
   }
-  return request
-}
+  return request;
+};
 
-const serializeCookies = cookieDict => {
-  let cookieString = ''
-  let i = 0
-  const cookieCount = Object.keys(cookieDict).length
+const serializeCookies = (cookieDict) => {
+  let cookieString = "";
+  let i = 0;
+  const cookieCount = Object.keys(cookieDict).length;
   for (const cookieName in cookieDict) {
-    const cookieValue = cookieDict[cookieName]
-    cookieString += cookieName + '=' + cookieValue
+    const cookieValue = cookieDict[cookieName];
+    cookieString += cookieName + "=" + cookieValue;
     if (i < cookieCount - 1) {
-      cookieString += '; '
+      cookieString += "; ";
     }
-    i++
+    i++;
   }
-  return cookieString
-}
+  return cookieString;
+};
 
 module.exports = {
   parseCurlCommand: parseCurlCommand,
-  serializeCookies: serializeCookies
-}
+  serializeCookies: serializeCookies,
+};
